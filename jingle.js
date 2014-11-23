0ad9dde7c40799bf62f2756f17ca6c774c0725fb
Blair Kelly <blairkelly1@gmail.com>
2014-11-23 16:27:24 -0500

    blink cycle

--- jingle.js
+++ jingle.js
@@ -5,3 +5,23 @@ var leds = ["P8_10"];
 
 console.log(leds);
 
+
+for(var i in leds) {
+    b.pinMode(leds[i], b.OUTPUT);
+}
+
+var state = b.LOW;
+for(var i in leds) {
+    b.digitalWrite(leds[i], state);
+}
+
+setInterval(toggle, 1000);
+
+function toggle() {
+    if(state == b.LOW) state = b.HIGH;
+    else state = b.LOW;
+    for(var i in leds) {
+        b.digitalWrite(leds[i], state);
+        console.log("pin: " + leds[i] + " set to " + state);
+    }
+}
